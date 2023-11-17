3ad0c375933dc45d41907bb32ffbd91b742bd46a
Filipko^^ <fiilgameslp@gmail.com>
2023-11-17 09:33:09 +0100

    switch to postgre

--- robocup-reg/web/settings.py
+++ robocup-reg/web/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "web.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "pg_service",
+            "passfile": ".pg_pass",
+        },
     }
 }
 
