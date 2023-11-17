56d19ae461672f5617e84d3008c10833bed03a2e
mbuchalova <buchalova6@uniba.sk>
2023-11-17 16:50:24 +0100

    problem with login

--- robocup-reg/web/users/forms.py
+++ robocup-reg/web/users/forms.py
@@ -1,12 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import BaseUserCreationForm, AuthenticationForm
 
 from web.users.models import RobocupUser
 
 
-class RegisterForm(UserCreationForm):
+class RegisterForm(BaseUserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = RobocupUser
         fields = ["email", "password1", "password2"]
+
+
+class LoginForm(AuthenticationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = RobocupUser
+        fields = ["username", "password"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Customize form labels, if needed
+        self.fields['email'].label = 'Email'
+        self.fields['password'].label = 'Password'
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        # Your custom validation logic here, e.g., checking if the user is active
+
+        return self.cleaned_data
--- robocup-reg/web/users/urls.py
+++ robocup-reg/web/users/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("login", views.login, name="login"), path("register", views.register, name="register")]
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [path("login", views.signin, name="login"), path("register", views.register, name="register")]
