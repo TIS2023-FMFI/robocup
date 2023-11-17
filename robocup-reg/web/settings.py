5af946ab7a5d57e79a18b85957cc6561be9e596b
buchalova <buchalova6@uniba.sk>
2023-11-17 13:26:04 +0100

    added registration form

--- robocup-reg/web/settings.py
+++ robocup-reg/web/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "web.common",
     "web.administrator",
     "web.leader",
@@ -136,3 +138,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# crispy-form settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- robocup-reg/web/users/views.py
+++ robocup-reg/web/users/views.py
@@ -1,6 +1,17 @@
-def login(request):
-    return None
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from .forms import RegisterForm
 
 
 def register(request):
-    return None
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "login.html", {"form": form})
