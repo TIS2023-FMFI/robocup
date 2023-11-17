6c172c3bd6a5ad1ccbc2c304a67b92cb8c32a836
Filipko^^ <fiilgameslp@gmail.com>
2023-11-17 12:30:56 +0100

    add simple sites for all modules

--- robocup-reg/web/administrator/urls.py
+++ robocup-reg/web/administrator/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("org-panel", views.org_panel, name="org-panel"),
+    path("admin-panel", views.admin_panel, name="admin-panel"),
 ]
---
+++ robocup-reg/web/users/views.py
@@ -0,0 +1,6 @@
+def login(request):
+    return None
+
+
+def register(request):
+    return None
