0d5d3b6c3f653d664e5143f9dd17b19276b33f8d
mbuchalova <buchalova6@uniba.sk>
2023-12-05 14:15:51 +0100

    added model Team

--- robocup-reg/web/users/models.py
+++ robocup-reg/web/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
 
 
 class RobocupUserManager(UserManager):
@@ -64,8 +65,8 @@ class Person(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=100, unique=True)
     birth_date = models.DateTimeField()
     primary_school = models.BooleanField()
     diet = models.CharField(default="Ziadna", max_length=100)
@@ -75,6 +76,17 @@ class Person(models.Model):
     food2 = models.BooleanField(default=True)
     food3 = models.BooleanField(default=True)
     supervisor = models.CharField(max_length=100)
+
+
+class Team(models.Model):
+    id = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=100, unique=True)
+    team_leader = models.IntegerField()
+    organization = models.CharField(max_length=100)
+    competitors = ArrayField(models.IntegerField())
+    categories = ArrayField(models.IntegerField())  # v ktorych kategoriach tim sutazi
+    category = models.BooleanField(default=True) # T - ZS, F - SS
+
 
 
 
