012fc6a1327392a274150363b6b2642e88fc0bcd
mbuchalova <buchalova6@uniba.sk>
2023-12-08 19:17:55 +0100

    added registration forms for competitor and supervisor

---
+++ robocup-reg/web/leader/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import BaseUserCreationForm
+
+from web.leader.models import Person, Team
+
+
+class RegisterFormCompetitor(BaseUserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Person
+        fields = ["email", "first_name", "last_nameb",
+                  "phone_number",
+                  "birth_date", "primary_school",
+                  "diet", "accomodation1", "accomodation2",
+                  "food1", "food2", "food3", "supervisor"]
+
+
+class RegisterFormSupervisor(BaseUserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Person
+        fields = ["first_name", "last_name", "email",
+                  "phone_number", "diet", "accomodation1",
+                  "accomodation2", "food1", "food2", "food3"]
