335dabec5a35ebb3305b841c7e83c5a5748e6938
mbuchalova <buchalova6@uniba.sk>
2023-12-10 16:30:29 +0100

    added registration form for category

--- robocup-reg/web/org/forms.py
+++ robocup-reg/web/org/forms.py
@@ -4,9 +4,20 @@ from web.org.models import Event, Category
 
 
 class RegisterFormEvent(forms.Form):
+    name = forms.CharField(required=True)
+    place = forms.CharField(required=True)
 
     class Meta:
         model = Event
         fieldsets = ["name", "place", "start_date", "end_date",
                      "registration_start_date", "registration_end_date",
                      "registration_open", "categories"]
+
+
+class RegisterFormCategory(forms.Form):
+    name = forms.CharField(required=True)
+
+    class Meta:
+        model = Category
+        fieldsets = ["name", "primary_school", "list_of_results", "soccer",
+                     "group_size", "advance", "ranking_params"]
