a8d37e659bba1875b42c8e51c7514948a97edf55
Filipko^^ <fiilgameslp@gmail.com>
2023-12-12 14:23:21 +0100

    send data to leader-panel

--- robocup-reg/web/leader/views.py
+++ robocup-reg/web/leader/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Person, Team
+
 
 def leader_panel(request):
-    return render(request, "leader-panel.html")
+    competitors = Person.objects.filter(_user_id=request.user.id, is_supervisor=False)
+    supervisors = Person.objects.filter(_user_id=request.user.id, is_supervisor=True)
+    teams = Team.objects.filter(_user_id=request.user.id)
+    data = {"competitors": competitors, "supervisors": supervisors, "teams": teams}
+
+    return render(request, "leader-panel.html", data)
