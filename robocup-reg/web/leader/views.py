cff4622065ae4d7e5849eeefa5bed9fd8097242c
mbuchalova <buchalova6@uniba.sk>
2023-12-18 10:59:59 +0100

    added editing competitor view

--- robocup-reg/web/leader/views.py
+++ robocup-reg/web/leader/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render
 
+from .forms import CompetitorForm
 from .models import Person, Team
 
 
@@ -9,3 +11,21 @@ def leader_panel(request):
     teams = Team.objects.filter(_user_id=request.user.id)
     data = {"competitors": competitors, "supervisors": supervisors, "teams": teams}
     return render(request, "leader-panel.html", data)
+
+
+def edit_competitor(request, id):
+    post = get_object_or_404(Person, id=id)
+
+    if request.method == "GET":
+        data = {"form": CompetitorForm(instance=post), "id": id}
+        return render(request, "competitors.html", data)
+
+    elif request.method == "POST":
+        form = CompetitorForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "The post has been updated successfully.")
+            return render(request, "competitors.html")
+        else:
+            messages.error(request, "Please correct the following errors:")
+            return render(request, "competitors.html", {"form": form})
