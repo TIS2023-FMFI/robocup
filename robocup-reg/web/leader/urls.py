8d23a0fe28bc048fca4931eef4abcda8501492d6
Oliver <sidor8@uniba.com>
2023-12-18 11:33:04 +0100

    fixed leader_panel href

--- robocup-reg/web/leader/urls.py
+++ robocup-reg/web/leader/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.leader_panel, name="leader_panel"),
-    path("leader_panel", views.leader_panel, name="leader_panel"),
+    path("leader-panel", views.leader_panel, name="leader_panel"),
     #    path("leader_panel/team_assembly", views.team_assembly, name="team_assembly"),
     #    path("leader_panel/add_supervisor", views.add_supervisor, name="add_supervisor"),
     #    path("leader_panel/add_competitor", views.add_competitor, name="add_competitor"),
