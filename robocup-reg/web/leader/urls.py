51058ce12f0a7fdf4a6e5ebf82a626a1ae178126
Filipko^^ <fiilgameslp@gmail.com>
2023-12-18 11:28:11 +0100

    comment urls to non existat views

--- robocup-reg/web/leader/urls.py
+++ robocup-reg/web/leader/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.leader_panel, name="leader_panel"),
     path("leader_panel", views.leader_panel, name="leader_panel"),
-    path("leader_panel/team_assembly", views.team_assembly, name="team_assembly"),
-    path("leader_panel/add_supervisor", views.add_supervisor, name="add_supervisor"),
-    path("leader_panel/add_competitor", views.add_competitor, name="add_competitor"),
+    #    path("leader_panel/team_assembly", views.team_assembly, name="team_assembly"),
+    #    path("leader_panel/add_supervisor", views.add_supervisor, name="add_supervisor"),
+    #    path("leader_panel/add_competitor", views.add_competitor, name="add_competitor"),
 ]
