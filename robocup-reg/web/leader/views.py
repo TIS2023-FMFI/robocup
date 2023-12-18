41d31ac48676a1bc1fde6f54aa93bc55f66750e2
mbuchalova <buchalova6@uniba.sk>
2023-12-18 11:12:00 +0100

    added editing view fpr supervisor and team

--- robocup-reg/web/leader/views.py
+++ robocup-reg/web/leader/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, render
 
-from .forms import CompetitorForm
+from .forms import CompetitorForm, SupervisorForm, TeamForm
 from .models import Person, Team
 
 
@@ -13,6 +13,16 @@ def leader_panel(request):
     return render(request, "leader-panel.html", data)
 
 
+def form_validation(form, request, html):
+    if form.is_valid():
+        form.save()
+        messages.success(request, "The post has been updated successfully.")
+        return render(request, f"{html}")
+    else:
+        messages.error(request, "Please correct the following errors:")
+        return render(request, f"{html}", {"form": form})
+
+
 def edit_competitor(request, id):
     post = get_object_or_404(Person, id=id)
 
@@ -22,10 +32,28 @@ def edit_competitor(request, id):
 
     elif request.method == "POST":
         form = CompetitorForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "The post has been updated successfully.")
-            return render(request, "competitors.html")
-        else:
-            messages.error(request, "Please correct the following errors:")
-            return render(request, "competitors.html", {"form": form})
+        form_validation(form, request, "competitors.html")
+
+
+def edit_supervisor(request, id):
+    post = get_object_or_404(Person, id=id)
+
+    if request.method == "GET":
+        data = {"form": SupervisorForm(instance=post), "id": id}
+        return render(request, "supervisors.html", data)
+
+    elif request.method == "POST":
+        form = SupervisorForm(request.POST, instance=post)
+        return form_validation(form, request, "supervisors.html")
+
+
+def edit_team(request, id):
+    post = get_object_or_404(Person, id=id)
+
+    if request.method == "GET":
+        data = {"form": TeamForm(instance=post), "id": id}
+        return render(request, "teams.html", data)
+
+    elif request.method == "POST":
+        form = TeamForm(request.POST, instance=post)
+        return form_validation(form, request, "teams.html")
