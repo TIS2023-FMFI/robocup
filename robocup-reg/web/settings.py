378e651fa330411c27768774bbff995fb686f63f
Filip Sršeň <fiilgameslp@gmail.com>
2024-02-09 13:03:07 +0100

    clean up settings from garbage

--- robocup-reg/web/settings.py
+++ robocup-reg/web/settings.py
@@ -42,6 +42,10 @@ TIME_ZONE = "Europe/Bratislava"
 
 AUTH_USER_MODEL = "users.RobocupUser"
 
+USE_I18N = True
+
+USE_TZ = True
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,18 +132,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
