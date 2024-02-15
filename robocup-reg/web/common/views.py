76d2076cb5e3c6ed2997657baab9e1381ed7f04d
mbuchalova <buchalova6@uniba.sk>
2024-02-15 12:39:09 +0100

    error in displaying results fixed

--- robocup-reg/web/common/views.py
+++ robocup-reg/web/common/views.py
@@ -16,7 +16,9 @@ def home(request):
 
 def results(request, id=1):
     teams = Team.objects.all()
-    categories = Category.objects.filter(event=Event.objects.filter(is_active=True).get()[0])
+    # print(Event.objects.filter(is_active=True).first())
+    # categories = Category.objects.filter(event=Event.objects.filter(is_active=True).get()[0])
+    categories = Category.objects.filter(event=Event.objects.filter(is_active=True).first())
     category_res = Category.objects.all().filter(id=id).values("results")
     data = {"teams": teams, "categories": categories, "category_results": category_res}
     return render(request, "results.html", data)
