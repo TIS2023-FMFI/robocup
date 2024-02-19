579a751b30b73e92dedb036e85edef5bcab5a248
Filipko^^ <fiilgameslp@gmail.com>
2024-02-19 19:56:06 +0100

    change entrypoint to docker and non-docker

--- robocup-reg/entrypoint.sh
+++ robocup-reg/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-export PATH=$PATH:/home/robocup/.local/bin
 set -euo pipefail
 
-pipenv run python3 manage.py migrate --force-color
-
-if [ "${1:-prod}" = "dev" ]; then
-  exec pipenv run python3 manage.py runserver 0.0.0.0:8042 --force-color
+if [ "${1:-prod}" = "docker" ]; then
+  python manage.py migrate --force-color
+  python manage.py collectstatic --no-input
+  exec gunicorn --bind 0.0.0.0:8000 --access-logfile - --log-file - web.wsgi
 else
+  export PATH=$PATH:/home/robocup/.local/bin
+  pipenv run python3 manage.py migrate --force-color
   pipenv run python3 manage.py collectstatic --no-input
   exec pipenv run gunicorn --bind 0.0.0.0:8042 --access-logfile - --log-file - web.wsgi
 fi
--- robocup-reg/web/settings.py
+++ robocup-reg/web/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "robocup.skse.sk"]
 
