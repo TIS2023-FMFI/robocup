3dea87210c18f15ad85c474d499702d988aa961f
Oliver <sidor8@uniba.com>
2024-02-21 05:01:05 +0100

    Updated results.html to differ in resilts showings by each category.

--- robocup-reg/web/common/views.py
+++ robocup-reg/web/common/views.py
@@ -17,20 +17,71 @@ def home(request):
 
 def results(request, id=1):
     teams = Team.objects.all()
-    # print(Event.objects.filter(is_active=True).first())
-    # categories = Category.objects.filter(event=Event.objects.filter(is_active=True).get()[0])
     categories = Category.objects.filter(event__is_active=True)
     selected_category = categories.filter(id=id).get()
     result_dict = selected_category.results
+    num_of_columns = len(result_dict[0].keys())
+    teams_in_cat = set()
+    ZS_teams = set()
+    SS_teams = set()
+    for team in teams:
+        for elem in team.categories.all():
+            if int(elem.id) == int(id):
+                teams_in_cat.add(team)
+
+    for team_ in teams_in_cat:
+        zs = True
+        for player in team_.competitors.all():
+            if not player.primary_school:
+                zs = False
+        if not zs:
+            SS_teams.add(team_.team_name)
+        else:
+            ZS_teams.add(team_.team_name)
     zoz = []
-    if result_dict:
-        for i in range(1, len(result_dict) + 1):
-            for prvok in result_dict:
-                if prvok["poradie"] == str(i):
-                    zoz.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
-    else:
-        messages.error(request, "Výsledky pre hľadanú kategóriu ešte neboli vyplnené.")
-    data = {"teams": teams, "categories": categories, "category_results": zoz, "selected_category": selected_category}
+    ss_cat_res = []
+    zs_cat_res = []
+    if selected_category.list_of_results == "COMB":
+        if result_dict:
+            for i in range(1, len(result_dict) + 1):
+                for prvok in result_dict:
+                    if prvok["poradie"] == str(i):
+                        if "body" in prvok.keys():
+                            zoz.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                        else:
+                            zoz.append([prvok["poradie"], prvok["nazov"]])
+        else:
+            messages.error(request, "Výsledky pre hľadanú kategóriu ešte neboli vyplnené.")
+
+    elif selected_category.list_of_results == "SEPR":
+        if result_dict:
+            for i in range(1, len(result_dict) + 1):
+                for prvok in result_dict:
+                    if prvok["nazov"] in ZS_teams:
+                        if prvok["poradie"] == str(i):
+                            if "body" in prvok.keys():
+                                zs_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                            else:
+                                zs_cat_res.append([prvok["poradie"], prvok["nazov"]])
+                    else:
+                        if prvok["poradie"] == str(i):
+                            if "body" in prvok.keys():
+                                ss_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                            else:
+                                ss_cat_res.append([prvok["poradie"], prvok["nazov"]])
+
+    data = {
+        "teams": teams,
+        "categories": categories,
+        "category_results": zoz,
+        "selected_category": selected_category,
+        "num_of_columns": num_of_columns,
+        "ZS_teams": ZS_teams,
+        "SS_teams": SS_teams,
+        "zs_cat_res": zs_cat_res,
+        "ss_cat_res": ss_cat_res,
+    }
+
     return render(request, "results.html", data)
 
 
