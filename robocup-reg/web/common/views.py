8ce6b3185abc5ae93e21b80d634493cb4e8d5bf7
Filipko^^ <fiilgameslp@gmail.com>
2024-03-09 13:36:52 +0100

    fix seperate team position

--- robocup-reg/web/common/views.py
+++ robocup-reg/web/common/views.py
@@ -35,13 +35,13 @@ def results(request, id=1):
 
     # ak to neni soccer
     if not selected_category.is_soccer:
-        
+
         if result_dict is None:
             result_dict = dict()
             num_of_columns = 0
         else:
             num_of_columns = len(result_dict[0].keys())
-        
+
         for team in teams:
             for elem in team.categories.all():
                 if int(elem.id) == int(id):
@@ -71,20 +71,24 @@ def results(request, id=1):
 
         elif selected_category.list_of_results == "SEPR":
             if result_dict:
+                zs_poradie = 1
+                ss_poradie = 1
                 for i in range(1, len(result_dict) + 1):
                     for prvok in result_dict:
                         if prvok["nazov"] in ZS_teams:
                             if prvok["poradie"] == str(i):
                                 if "body" in prvok.keys():
-                                    zs_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                                    zs_cat_res.append([zs_poradie, prvok["nazov"], prvok["body"]])
                                 else:
-                                    zs_cat_res.append([prvok["poradie"], prvok["nazov"]])
+                                    zs_cat_res.append([zs_poradie, prvok["nazov"]])
+                                zs_poradie += 1
                         else:
                             if prvok["poradie"] == str(i):
                                 if "body" in prvok.keys():
-                                    ss_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                                    ss_cat_res.append([ss_poradie, prvok["nazov"], prvok["body"]])
                                 else:
-                                    ss_cat_res.append([prvok["poradie"], prvok["nazov"]])
+                                    ss_cat_res.append([ss_poradie, prvok["nazov"]])
+                                    ss_poradie += 1
             else:
                 messages.error(request, "Výsledky pre hľadanú kategóriu ešte neboli vyplnené.")
         else:
@@ -96,24 +100,28 @@ def results(request, id=1):
                                 zoz.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
                             else:
                                 zoz.append([prvok["poradie"], prvok["nazov"]])
+                zs_poradie = 1
+                ss_poradie = 1
                 for i in range(1, len(result_dict) + 1):
                     for prvok in result_dict:
                         if prvok["nazov"] in ZS_teams:
                             if prvok["poradie"] == str(i):
                                 if "body" in prvok.keys():
-                                    zs_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                                    zs_cat_res.append([zs_poradie, prvok["nazov"], prvok["body"]])
                                 else:
-                                    zs_cat_res.append([prvok["poradie"], prvok["nazov"]])
+                                    zs_cat_res.append([zs_poradie, prvok["nazov"]])
+                                zs_poradie += 1
                         else:
                             if prvok["poradie"] == str(i):
                                 if "body" in prvok.keys():
-                                    ss_cat_res.append([prvok["poradie"], prvok["nazov"], prvok["body"]])
+                                    ss_cat_res.append([ss_poradie, prvok["nazov"], prvok["body"]])
                                 else:
-                                    ss_cat_res.append([prvok["poradie"], prvok["nazov"]])
+                                    ss_cat_res.append([ss_poradie, prvok["nazov"]])
+                                    ss_poradie += 1
             else:
                 messages.error(request, "Výsledky pre hľadanú kategóriu ešte neboli vyplnené.")
 
-     # ak to je soccer
+    # ak to je soccer
     elif selected_category.is_soccer:
         if result_dict:
             result_dict = json.loads(selected_category.results)
@@ -122,41 +130,43 @@ def results(request, id=1):
                 teams_stats = {}
                 for match_teams, match_result in matches.items():
                     # print(f"match_result: {match_result}")
-                    team_a, team_b = match_teams.split('-')[0:2]
+                    team_a, team_b = match_teams.split("-")[0:2]
                     # Initialize or update team stats
                     for team in [team_a, team_b]:
                         if team not in teams_stats:
-                            teams_stats[team] = {'P': 0, 'S': [0, 0], 'W': 0, 'D': 0, 'L': 0, 'Pts': 0}
+                            teams_stats[team] = {"P": 0, "S": [0, 0], "W": 0, "D": 0, "L": 0, "Pts": 0}
 
-                    if match_result == None or match_result == 'None':
+                    if match_result is None or match_result == "None":
                         continue
-                    score_a, score_b = map(int, match_result.split(':'))
+                    score_a, score_b = map(int, match_result.split(":"))
 
                     # Update stats
-                    teams_stats[team_a]['P'] += 1
-                    teams_stats[team_b]['P'] += 1
-                    teams_stats[team_a]['S'][0] += score_a
-                    teams_stats[team_a]['S'][1] += score_b
-                    teams_stats[team_b]['S'][0] += score_b
-                    teams_stats[team_b]['S'][1] += score_a
+                    teams_stats[team_a]["P"] += 1
+                    teams_stats[team_b]["P"] += 1
+                    teams_stats[team_a]["S"][0] += score_a
+                    teams_stats[team_a]["S"][1] += score_b
+                    teams_stats[team_b]["S"][0] += score_b
+                    teams_stats[team_b]["S"][1] += score_a
 
                     # Win, draw, loss logic
                     if score_a > score_b:
-                        teams_stats[team_a]['W'] += 1
-                        teams_stats[team_b]['L'] += 1
-                        teams_stats[team_a]['Pts'] += 3
+                        teams_stats[team_a]["W"] += 1
+                        teams_stats[team_b]["L"] += 1
+                        teams_stats[team_a]["Pts"] += 3
                     elif score_a < score_b:
-                        teams_stats[team_b]['W'] += 1
-                        teams_stats[team_a]['L'] += 1
-                        teams_stats[team_b]['Pts'] += 3
+                        teams_stats[team_b]["W"] += 1
+                        teams_stats[team_a]["L"] += 1
+                        teams_stats[team_b]["Pts"] += 3
                     else:
-                        teams_stats[team_a]['D'] += 1
-                        teams_stats[team_b]['D'] += 1
-                        teams_stats[team_a]['Pts'] += 1
-                        teams_stats[team_b]['Pts'] += 1
+                        teams_stats[team_a]["D"] += 1
+                        teams_stats[team_b]["D"] += 1
+                        teams_stats[team_a]["Pts"] += 1
+                        teams_stats[team_b]["Pts"] += 1
 
                 # Sort teams by points, then goal difference, then goals scored
-                group_results[group] = sorted(teams_stats.items(), key=lambda x: (-x[1]['Pts'], -(x[1]['S'][0] - x[1]['S'][1]), -x[1]['S'][0]))
+                group_results[group] = sorted(
+                    teams_stats.items(), key=lambda x: (-x[1]["Pts"], -(x[1]["S"][0] - x[1]["S"][1]), -x[1]["S"][0])
+                )
 
     data = {
         "teams": teams,
@@ -248,4 +258,3 @@ def download_teams(request):
 
 def detailed_results(request, id):
     return HttpResponse(Category.objects.filter(id=id).get().detailed_pdf, content_type="application/pdf")
-
